e57d774d1442190a32facd9274498d939d8ab937
SamcraftSam <sashatim2006@gmail.com>
2024-11-06 09:45:58 +0200

    commit

--- log.h
+++ log.h
@@ -21,13 +21,13 @@
         #define COLOR_MAGENTA "\033[35m"
         #define COLOR_RESET "\033[0m"
 
-        #if LOGGING_VERBOSITY >= 1
+        #if LOGGING_VERBOSITY == 5
         #define LOGV(fmt, ...) LOGGING_CLASS.printf(COLOR_YELLOW "[VERBOSE] " fmt COLOR_RESET "\n", ##__VA_ARGS__)
         #else
         #define LOGV(fmt, ...)
         #endif
 
-        #if LOGGING_VERBOSITY >= 2
+        #if LOGGING_VERBOSITY >= 4
         #define LOGD(fmt, ...) LOGGING_CLASS.printf(COLOR_MAGENTA "[DEBUG] " fmt COLOR_RESET "\n", ##__VA_ARGS__)
         #else
         #define LOGD(fmt, ...)
@@ -39,26 +39,26 @@
         #define LOGI(fmt, ...)
         #endif
 
-        #if LOGGING_VERBOSITY >= 4
+        #if LOGGING_VERBOSITY >= 2
         #define LOGW(fmt, ...) LOGGING_CLASS.printf(COLOR_YELLOW "[WARNING] " fmt COLOR_RESET "\n", ##__VA_ARGS__)
         #else
         #define LOGW(fmt, ...)
         #endif
 
-        #if LOGGING_VERBOSITY >= 5
+        #if LOGGING_VERBOSITY >= 1
         #define LOGE(fmt, ...) LOGGING_CLASS.printf(COLOR_RED "[ERROR] " fmt COLOR_RESET "\n", ##__VA_ARGS__)
         #else
         #define LOGE(fmt, ...)
         #endif
 
     #else
-        #if LOGGING_VERBOSITY >= 1
+        #if LOGGING_VERBOSITY >= 5
         #define LOGV(fmt, ...) LOGGING_CLASS.printf("[VERBOSE] " fmt "\n", ##__VA_ARGS__)
         #else
         #define LOGV(fmt, ...)
         #endif
 
-        #if LOGGING_VERBOSITY >= 2
+        #if LOGGING_VERBOSITY >= 4
         #define LOGD(fmt, ...) LOGGING_CLASS.printf("[DEBUG] " fmt "\n", ##__VA_ARGS__)
         #else
         #define LOGD(fmt, ...)
@@ -70,13 +70,13 @@
         #define LOGI(fmt, ...)
         #endif
 
-        #if LOGGING_VERBOSITY >= 4
+        #if LOGGING_VERBOSITY >= 2
         #define LOGW(fmt, ...) LOGGING_CLASS.printf("[WARNING] " fmt "\n", ##__VA_ARGS__)
         #else
         #define LOGW(fmt, ...)
         #endif
 
-        #if LOGGING_VERBOSITY >= 5
+        #if LOGGING_VERBOSITY >= 1
         #define LOGE(fmt, ...) LOGGING_CLASS.printf("[ERROR] " fmt "\n", ##__VA_ARGS__)
         #else
         #define LOGE(fmt, ...)
